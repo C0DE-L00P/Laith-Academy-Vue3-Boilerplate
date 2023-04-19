48d798b5852fdccf265e81e85222053d985e6604
Mohamed Gamal <mohamedgyaseen@gmail.com>
2023-04-19 12:55:54 +0200

    first commit

---
+++ 1_counter.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <div>0</div>
+        <div>
+            <button>-</button>
+            <button style="margin-inline: 4px;">+</button>
+        </div>
+    </main>
+</template>
+  
+<style scoped>
+main {
+    display: flex;
+    height: 100vh;
+    place-content: center;
+    flex-direction: column;
+    gap: 8px;
+    align-items: center;
+}
+</style>
---
+++ 4_p2_car-view.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        <h1>Car View</h1>
+        <p>Car Name</p>
+        <p>Car Year</p>
+        <p>Car Price $</p>
+    </main>
+
+    <contact-info>
+        <h1>Contact Info</h1>
+        <p>Contact Dealer</p>
+        <p>Contact Number</p>
+    </contact-info>
+</template>
